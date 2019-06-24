af31ba49847a38b01f41cae280e18624995495e7
secg1 <secg1@DESKTOP-4QUEOV1>
2019-06-24 15:40:04 -0600

    Initial commit, added dependencies and the initial configuration for springMVC with JPA

---
+++ cine/src/com/uca/cine/configuration/JpaConfiguration.java
@@ -0,0 +1,62 @@
+package com.uca.cine.configuration;
+
+ import java.util.Properties;
+
+ import javax.persistence.EntityManagerFactory;
+ import javax.sql.DataSource;
+
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+ import org.springframework.jdbc.datasource.DriverManagerDataSource;
+ import org.springframework.orm.jpa.JpaTransactionManager;
+ import org.springframework.orm.jpa.JpaVendorAdapter;
+ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+ import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+ @Configuration
+ @EnableTransactionManagement
+ @EnableJpaRepositories(basePackages = "com.uca.cine.repository")
+ public class JpaConfiguration {
+	 
+	 @Bean
+	 public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	 LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+	 em.setDataSource(dataSource());
+	 em.setPersistenceUnitName("cine");
+	 em.setPackagesToScan("com.uca.cine.domain");
+
+	 JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	 em.setJpaVendorAdapter(vendorAdapter);
+	 em.setJpaProperties(hibernateProperties());
+	 return em;
+}
+
+ @Bean
+ JpaTransactionManager transactionManager (EntityManagerFactory entityManagerFactory) {
+	 JpaTransactionManager transactionManager = new JpaTransactionManager();
+	 transactionManager.setEntityManagerFactory(entityManagerFactory);
+	 return transactionManager;
+ }
+
+
+ @Bean
+ public DataSource dataSource(){
+ DriverManagerDataSource dataSource = new DriverManagerDataSource();
+ dataSource.setDriverClassName("org.postgresql.Driver");
+ dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/proyectoNCapas");
+ dataSource.setUsername("postgres");
+ dataSource.setPassword("root");
+ return dataSource;
+ }
+
+ Properties hibernateProperties() {
+ Properties properties = new Properties();
+ properties.setProperty("hibernate.show_sql", "true");
+ properties.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
+ properties.setProperty("hibernate.enable_lazy_load_no_trans","true");
+ return properties;
+
+ }
+}
